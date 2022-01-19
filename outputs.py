a3a7e113ef73854f98ddf5885a684f51d67aa2eb
Hemantbt21 <96773002+Hemantbt21@users.noreply.github.com>
2022-01-19 16:27:40 +0530

    Add files via upload

---
+++ outputs.py
@@ -0,0 +1,25 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============= RESTART: C:/Users/DELL IN/Desktop/New folder/ques1.py ============
+enter first number:20
+enter second number:30
+enter third number:40
+The average of three numbers is: 30.0
+
+============= RESTART: C:/Users/DELL IN/Desktop/New folder/ques2.py ============
+Enter your Gross income:100000000
+Enter number of dependents:4
+income tax is: 19995600.0
+
+============= RESTART: C:/Users/DELL IN/Desktop/New folder/ques3.py ============
+['enter sid', 'km', 'm', 'Electrical Engineering', 10]
+
+============= RESTART: C:/Users/DELL IN/Desktop/New folder/ques4.py ============
+[69, 79, 88, 89]
+
+============ RESTART: C:/Users/DELL IN/Desktop/New folder/ques5a.py ============
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+
+============ RESTART: C:/Users/DELL IN/Desktop/New folder/ques5b.py ============
+['Red', 'Green', 'White', 'Purple', 'Yellow']
